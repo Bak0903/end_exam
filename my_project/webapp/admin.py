922f385aa7e7434266fbd65963bbd9a611e6a9be
bak0903 <bak0903@mail.ru>
2019-06-29 18:47:30 +0600

    Небольшие фиксы.

--- my_project/webapp/admin.py
+++ my_project/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from webapp.models import Author, Book
+from webapp.models import Author, Book, Bookcase
 
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(Bookcase)
 
