ca01513d3fc90546b2c11a86677d47af998fe30e
bak0903 <bak0903@mail.ru>
2019-06-29 09:39:43 +0600

    Этап 1. Добавил модель автора и провел миграцию.

--- my_project/webapp/models.py
+++ my_project/webapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Автор')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    death_date = models.DateField(null=True, blank=True, verbose_name='Дата смерти')
+    biography = models.TextField(null=True, blank=True, max_length=2000, verbose_name='Биография')
