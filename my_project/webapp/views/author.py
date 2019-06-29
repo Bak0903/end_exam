b0d91067914be3578260e49f642d0c1e2f64c36d
bak0903 <bak0903@mail.ru>
2019-06-29 14:59:51 +0600

    Этап 2. Разделил представления, добавил CRUD для книг и скачивание.

--- my_project/webapp/views/author.py
+++ my_project/webapp/views/author.py
@@ -1,17 +1,13 @@
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from webapp.models import Author
+from webapp.models import Author, Book
 from webapp.forms import AuthorForm
-
-
-class BaseView(LoginRequiredMixin, TemplateView):
-    template_name = 'base.html'
 
 
 class AuthorListView(ListView):
     model = Author
-    template_name = 'Author/list.html'
+    template_name = 'author/author_list.html'
 
     def get_queryset(self):
         return Author.objects.active()
@@ -19,15 +15,20 @@ class AuthorListView(ListView):
 
 class AuthorDetailView(DetailView):
     model = Author
-    template_name = 'Author/detail.html'
+    template_name = 'author/author_detail.html'
 
     def get_queryset(self):
         return Author.objects.active()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(author=self.object.pk, is_deleted=False)
+        return context
+
 
 class AuthorCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Author
-    template_name = 'Author/create.html'
+    template_name = 'author/author_create.html'
     form_class = AuthorForm
 
     def get_permission_required(self):
@@ -39,7 +40,7 @@ class AuthorCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
 
 class AuthorUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Author
-    template_name = 'Author/update.html'
+    template_name = 'author/author_update.html'
     form_class = AuthorForm
 
     def get_queryset(self):
---
+++ my_project/webapp/views/book.py
@@ -0,0 +1,63 @@
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from webapp.models import Author, Book
+from webapp.forms import BookForm
+
+
+class BookDetailView(DetailView):
+    model = Author
+    template_name = 'book/book_detail.html'
+
+    def get_queryset(self):
+        return Book.objects.active()
+
+
+class BookCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Book
+    template_name = 'book/book_create.html'
+    form_class = BookForm
+
+    def get_permission_required(self):
+        return None
+
+    def has_permission(self):
+        return self.request.user.is_staff
+
+
+class BookUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Book
+    template_name = 'book/book_update.html'
+    form_class = BookForm
+
+    def get_queryset(self):
+        return Book.objects.active()
+
+    def get_permission_required(self):
+        return None
+
+    def has_permission(self):
+        return self.request.user.is_staff
+
+
+def delete_book(request, book_pk):
+    book = get_object_or_404(Book, pk=book_pk)
+    if request.user.is_staff:
+        book.is_deleted = True
+        book.save()
+    return redirect('webapp:author', pk=book.author.pk)
+
+
+def download_file(request, book_pk):
+    book = get_object_or_404(Book, pk = book_pk)
+    path = book.file.path
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/file")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
