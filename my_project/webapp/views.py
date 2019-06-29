4b61ed1ab1b236cc776f36a801ddc4a93fc6a90f
bak0903 <bak0903@mail.ru>
2019-06-29 12:11:32 +0600

    Этап 1. Настроил доступ к добавлению, редактированию и удалению автора.

--- my_project/webapp/views.py
+++ my_project/webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from webapp.models import Author
 from webapp.forms import AuthorForm
 
@@ -24,13 +24,20 @@ class AuthorDetailView(DetailView):
     def get_queryset(self):
         return Author.objects.active()
 
-class AuthorCreateView(CreateView):
+
+class AuthorCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Author
     template_name = 'Author/create.html'
     form_class = AuthorForm
 
+    def get_permission_required(self):
+        return None
 
-class AuthorUpdateView(UpdateView):
+    def has_permission(self):
+        return self.request.user.is_staff
+
+
+class AuthorUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Author
     template_name = 'Author/update.html'
     form_class = AuthorForm
@@ -38,9 +45,16 @@ class AuthorUpdateView(UpdateView):
     def get_queryset(self):
         return Author.objects.active()
 
+    def get_permission_required(self):
+        return None
+
+    def has_permission(self):
+        return self.request.user.is_staff
+
 
 def delete_author(request, author_pk):
-    author = get_object_or_404(Author, pk=author_pk)
-    author.is_deleted = True
-    author.save()
+    if request.user.is_staff:
+        author = get_object_or_404(Author, pk=author_pk)
+        author.is_deleted = True
+        author.save()
     return redirect('webapp:authors')
