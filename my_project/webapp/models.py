ec47d9452ddd46aa73f10e291d053af7531de9b2
bak0903 <bak0903@mail.ru>
2019-06-29 12:37:53 +0600

    Этап 2. Добавил модель книги.

--- my_project/webapp/models.py
+++ my_project/webapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 
 class SoftDeleteManager(models.Manager):
@@ -23,3 +24,18 @@ class Author(models.Model):
 
     def get_absolute_url(self):
         return reverse('webapp:author', kwargs={'pk': self.pk})
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, verbose_name='Автор')
+    published_year = models.CharField(max_length=4, null=True, blank=True, verbose_name='Год издания')
+    file = models.FileField(null=True, blank=True, verbose_name='Файл')
+    picture = models.ImageField(null=True, blank=True, verbose_name='Обложка')
+    description = models.TextField(null=True, blank=True, max_length=2000, verbose_name='Описание')
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.published_year)
+
+    def get_absolute_url(self):
+        return reverse('webapp:book', kwargs={'pk': self.pk})
