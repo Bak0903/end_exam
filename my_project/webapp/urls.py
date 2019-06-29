449b6d06902981fb233ffecdbfc2e436d8a0a0c3
bak0903 <bak0903@mail.ru>
2019-06-29 15:01:41 +0600

    Этап 2. Добавил ссылки для CRUD книг и для скачивания.

--- my_project/webapp/urls.py
+++ my_project/webapp/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
-from webapp.views import BaseView, AuthorListView, AuthorDetailView, AuthorCreateView, AuthorUpdateView, delete_author
+from webapp.views.author import AuthorListView, AuthorDetailView, AuthorCreateView, AuthorUpdateView, delete_author
+from webapp.views.book import BookDetailView, BookCreateView, BookUpdateView, delete_book, download_file
 
 app_name = 'webapp'
 
 urlpatterns = [
-    path('', BaseView.as_view(), name='base'),
     path('authors/', AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', AuthorDetailView.as_view(), name='author'),
     path('author/create/', AuthorCreateView.as_view(), name='create_author'),
     path('author/<int:pk>/update/', AuthorUpdateView.as_view(), name='update_author'),
-    path('author/<int:author_pk>/delete/', delete_author, name='delete_author')
+    path('author/<int:author_pk>/delete/', delete_author, name='delete_author'),
+
+    path('book/<int:pk>', BookDetailView.as_view(), name='book'),
+    path('book/create/', BookCreateView.as_view(), name='create_book'),
+    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='update_book'),
+    path('book/<int:book_pk>/delete/', delete_book, name='delete_book'),
+    path('download/<int:book_pk>', download_file, name='download')
 ]
