4fdc64895ac74b053d2036200b07a551c82367c0
bak0903 <bak0903@mail.ru>
2019-06-29 11:56:15 +0600

    Этап 1. Добавил ссылки для CRUD автора.

--- my_project/webapp/urls.py
+++ my_project/webapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from webapp.views import BaseView
+from webapp.views import BaseView, AuthorListView, AuthorDetailView, AuthorCreateView, AuthorUpdateView, delete_author
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
+    path('authors/', AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', AuthorDetailView.as_view(), name='author'),
+    path('author/create/', AuthorCreateView.as_view(), name='create_author'),
+    path('author/<int:pk>/update/', AuthorUpdateView.as_view(), name='update_author'),
+    path('author/<int:author_pk>/delete/', delete_author, name='delete_author')
 ]
