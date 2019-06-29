972628acfa631d8aad0600256b5e2c903452dcfd
bak0903 <bak0903@mail.ru>
2019-06-29 11:55:21 +0600

    Этап 1. Добавил представления CRUD для автора.

--- my_project/webapp/views.py
+++ my_project/webapp/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from webapp.models import Author
+from webapp.forms import AuthorForm
 
-# Create your views here.
+
+class BaseView(LoginRequiredMixin, TemplateView):
+    template_name = 'base.html'
+
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'Author/list.html'
+
+    def get_queryset(self):
+        return Author.objects.active()
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'Author/detail.html'
+
+    def get_queryset(self):
+        return Author.objects.active()
+
+class AuthorCreateView(CreateView):
+    model = Author
+    template_name = 'Author/create.html'
+    form_class = AuthorForm
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    template_name = 'Author/update.html'
+    form_class = AuthorForm
+
+    def get_queryset(self):
+        return Author.objects.active()
+
+
+def delete_author(request, author_pk):
+    author = get_object_or_404(Author, pk=author_pk)
+    author.is_deleted = True
+    author.save()
+    return redirect('webapp:authors')
